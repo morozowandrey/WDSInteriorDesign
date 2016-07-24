acac20980edd7424d36cf3f00b0f7f4e101f5c40
Andrew Morozow <andrey940@gmail.com>
2016-07-24 18:34:15 +0300

    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+module.exports = function(grunt) {
+
+    // 1. Вся настройка находится здесь
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            dist: {
+                src: [
+                    'src/js/libs/*.js', // Все JS в папке libs
+                    'src/js/modules/*.js', // Все JS в папке libs
+                    'src/js/global.js'  // Конкретный файл
+                ],
+                dest: 'js/build/production.js',
+            }
+        },
+        uglify: {
+            build: {
+                src: 'js/build/production.js',
+                dest: 'js/build/production.min.js'
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'images/build/'
+                }]
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    'css/build/main.css': 'src/sass/main.scss'
+                }
+            }
+        },
+        css: {
+            files: ['src/sass/*.scss'],
+            tasks: ['sass'],
+            options: {
+                spawn: false,
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['src/js/*.js'],
+                tasks: ['concat','uglify'],
+                options: {
+                    spawn: false,
+                    livereload: true
+                },
+            },
+            sass: {
+                files: ['src/sass/**/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false,
+                    livereload: true
+                },
+            },
+        }
+
+    });
+
+    // 3. Тут мы указываем Grunt, что хотим использовать этот плагин
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // 4. Указываем, какие задачи выполняются, когда мы вводим «grunt» в терминале
+    grunt.registerTask('default', ['concat', 'uglify', 'imagemin','sass', 'watch']);
+
+};
---
+++ src/js/modules/googlemap.js
@@ -0,0 +1,16 @@
+function initialize() {
+	var latlng = new google.maps.LatLng(50.4501, 30.5234);
+	var settings = {
+		zoom: 15,
+		center: latlng,
+		mapTypeControl: true,
+		mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
+		navigationControl: true,
+		navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
+		mapTypeId: google.maps.MapTypeId.ROADMAP};
+	var map = new google.maps.Map(document.getElementById("map_canvas"), settings);
+				
+	google.maps.event.addListener(companyMarker, 'click', function() {
+	infowindow.open(map,companyMarker);
+	});
+}
